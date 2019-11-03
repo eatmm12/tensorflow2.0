8405e891e036bbf33827ff199a91b57ba14a57cd
何洋洋 <heyangyang@jd.com>
2019-11-03 23:01:10 +0800

    add text classificaton with hub

--- Text classification with TensorFlow Hub.py
+++ Text classification with TensorFlow Hub.py
@@ -2,12 +2,9 @@
 # @Time    : 2019-10-30 00:03
 # @Author  : Heyangyang
 
-import numpy as np
-
 import tensorflow as tf
-
-import tensorflow_hub as hub
 import tensorflow_datasets as tfds
+import tensorflow_hub as hub
 
 print("Version: ", tf.__version__)
 print("Eager mode: ", tf.executing_eagerly())
@@ -20,3 +17,33 @@ train_validation_split = tfds.Split.TRAIN.subsplit([6, 4])
     name="imdb_reviews",
     split=(train_validation_split, tfds.Split.TEST),
     as_supervised=True)
+
+train_example_batch, train_label_batch = next(iter(train_data.batch(10)))
+# print(train_example_batch)
+# print(train_label_batch)
+
+embedding = "https://hub.tensorflow.google.cn/google/tf2-preview/gnews-swivel-20dim-with-oov/1"
+hub_layer = hub.KerasLayer(embedding, input_shape=[],
+                           dtype=tf.string, trainable=True)
+print(hub_layer(train_example_batch[:3]))
+
+model = tf.keras.Sequential()
+model.add(hub_layer)
+model.add(tf.keras.layers.Dense(16, activation='relu'))
+model.add(tf.keras.layers.Dense(1, activation='sigmoid'))
+
+print(model.summary())
+
+model.compile(optimizer='adam',
+              loss='binary_crossentropy',
+              metrics=['accuracy'])
+
+history = model.fit(train_data.shuffle(10000).batch(512),
+                    epochs=20,
+                    validation_data=validation_data.batch(512),
+                    verbose=1)
+
+results = model.evaluate(test_data.batch(512), verbose=2)
+
+for name, value in zip(model.metrics_names, results):
+    print("%s: %.3f" % (name, value))
